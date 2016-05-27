e3c01a35a7ab5a7dc1bc068a736e05d3193ae7f6
asagi00a3af <shimo.8810@gmail.com>
2016-05-27 10:58:50 +0900

    add test dir

--- fca.py
+++ fca.py
@@ -1,1 +1,20 @@
+import pandas as pd
+import copy
 
+def intuitive_generation(context):
+    '''
+    直感的コンセプトラティス生成アルゴリズム
+    argument:
+        context: pandas.core.frame.DataFrameのcontextテーブル
+    return:
+        lattice: 考え中
+    '''
+
+def next_closure_generation(context):
+    '''
+    NextClosureコンセプトラティス生成アルゴリズム
+    argument:
+        context: pandas.core.frame.DataFrameのcontextテーブル
+    return:
+        lattice: 考え中
+    '''
